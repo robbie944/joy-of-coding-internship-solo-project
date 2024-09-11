be38beee9ad5ad21714c00d10c019eb6a78e968e
Robin Chandler <robin.chandler944@gmail.com>
2024-09-11 05:14:53 -0600

    Chapter one Initial commit

---
+++ web/src/pages/HomePage/HomePage.js
@@ -0,0 +1,14 @@
+
+import { Metadata } from '@redwoodjs/web'
+
+const HomePage = () => {
+  return (
+    <>
+      <Metadata title="Home" description="Home page" />
+<>Home</>
+
+    </>
+  )
+}
+
+export default HomePage
