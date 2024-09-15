417e76f6573a44d45a242716b6d6217125ff6937
Robin Chandler <robin.chandler944@gmail.com>
2024-09-14 23:47:45 -0600

    Chapter 3 commit

--- web/src/Routes.js
+++ web/src/Routes.js
@@ -15,7 +15,9 @@ import BlogLayout from './layouts/BlogLayout/BlogLayout'
 const Routes = () => {
   return (
     <Router>
-      
+
+
+
       <Set wrap={ScaffoldLayout} title="Posts" titleTo="posts" buttonLabel="New Post" buttonTo="newPost">
         <Route path="/posts/new" page={PostNewPostPage} name="newPost" />
         <Route path="/posts/{id:Int}/edit" page={PostEditPostPage} name="editPost" />
@@ -24,6 +26,7 @@ const Routes = () => {
       </Set>
       <Set wrap={BlogLayout}>
       <Route path="/about" page={AboutPage} name="about" />
+      <Route path="/contact" page={ContactPage} name="contact" />
       <Route path="/" page={HomePage} name="home" />
       <Route path="/article/{id:Int}" page={ArticlePage} name="article" />
       </Set>
---
+++ web/src/pages/ContactPage/ContactPage.js
@@ -0,0 +1,65 @@
+import { Form, TextField, TextAreaField, Submit, FieldError, Label, useForm, FormError} from '@redwoodjs/forms'
+import { Metadata, useMutation } from '@redwoodjs/web'
+import { Toaster, toast} from '@redwoodjs/web/toast'
+
+const CREATE_CONTACT = gql`
+  mutation CreateContactMutation($input: CreateContactInput!) {
+    createContact(input: $input) {
+      id
+    }
+  }
+`
+
+const ContactPage = () => {
+  const formMethods = useForm()
+  const [create, { loading, error}] = useMutation(CREATE_CONTACT, {onCompleted: () =>{
+    toast.success('Thank you for your message!')
+    formMethods.reset()
+  }
+  })
+
+  const onSubmit =(data) => {
+    console.log(data)
+    create ({
+      variables: {
+        input: data
+
+      }
+    })
+  }
+  return (
+    <>
+      <Metadata title="Contact" description="Contact page" />
+
+      <Toaster />
+      <Form onSubmit={onSubmit} config={{ mode: 'onBlur' }} error={error}>
+        <FormError error={error} wrapperClassName="form-error" />
+
+        <Label name="name" errorClassName='error'>Name</Label>
+        <TextField name="name" errorClassName='error' validation={{required:true}}/>
+        <FieldError name ="name" className='error' />
+
+        <Label name="email" errorClassName="error">
+          Email
+        </Label>
+        <TextField
+          name="email"
+          validation={{
+            required: true,
+          }}
+          errorClassName="error"
+        />
+        <FieldError name="email" className="error" />
+
+        <Label name="message" errorClassName='error'>Message</Label>
+        <TextAreaField name="message" errorClassName='error' validation={{required:true}}/>
+        <FieldError name ="message" className='error' />
+
+        <Submit disabled={loading}>Send Message</Submit>
+
+      </Form>
+    </>
+  )
+}
+
+export default ContactPage
