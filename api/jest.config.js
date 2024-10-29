0f85bed8a33ea2f68f6124afaebb00e77ec45adc
Robin Chandler <robin.chandler944@gmail.com>
2024-10-28 23:51:12 -0600

    Tutorial complete with functionality but with 5 test errors

--- api/jest.config.js
+++ api/jest.config.js
@@ -6,3 +6,13 @@ const config = {
 }
 
 module.exports = config
+
+// api/jest.config.js
+// const config = {
+//   rootDir: '../',
+//   preset: '@redwoodjs/testing/config/jest/api',
+//   setupFilesAfterEnv: ['<rootDir>/src/lib/jest.setup.js'], // Add this line
+// }
+
+// module.exports = config
+
--- api/src/graphql/posts.sdl.js
+++ api/src/graphql/posts.sdl.js
@@ -1,4 +1,4 @@
-export const schema = gql`
+ export const schema = gql`
   type Post {
     id: Int!
     title: String!
@@ -10,21 +10,5 @@ export const schema = gql`
   type Query {
     posts: [Post!]! @skipAuth
     post(id: Int!): Post @skipAuth
-  }
-
-  input CreatePostInput {
-    title: String!
-    body: String!
-  }
-
-  input UpdatePostInput {
-    title: String
-    body: String
-  }
-
-  type Mutation {
-    createPost(input: CreatePostInput!): Post! @requireAuth
-    updatePost(id: Int!, input: UpdatePostInput!): Post! @requireAuth
-    deletePost(id: Int!): Post! @requireAuth(roles: ["moderator", "admin"])
   }
 `
